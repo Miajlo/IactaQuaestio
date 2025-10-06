89bf2f2627e5e62c853856905b61f933ada27ec8
Miajlo <mihajlod.mes@gmail.com>
2025-10-06 02:41:20 +0200

    Mongo and PyTesseract working.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from urllib.parse import quote_plus
+import pytesseract
+from PIL import Image
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import os
+
+from pymongo.server_api import ServerApi
 
 if __name__ == "__main__":
-    print("A");
+    #load_dotenv()
+
+    image = Image.open('TIKStest.jpg')
+
+    text = pytesseract.image_to_string(image, lang='eng')  # English
+    print(text)
+
+    # username = os.getenv("MONGO_USER")
+    # password = quote_plus(os.getenv("MONGO_PASSWORD"))
+    #
+    # uri = f"mongodb+srv://{username}:{password}@masterlibrarian.bforp68.mongodb.net/?retryWrites=true&w=majority&appName=MasterLibrarian"
+    #
+    # client = MongoClient(uri, server_api=ServerApi('1'))
+    # # Send a ping to confirm a successful connection
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+    # except Exception as e:
+    #     print(e)
